9c6e27cb84e6e792c55963bab99c074da61413c4
donnybrilliant <daniel.vier@gmail.com>
2023-09-30 15:06:17 +0200

    Updates Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
+import { StyledHeader, Logo, MenuContainer, MenuButton } from "./Header.styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard, faBars } from "@fortawesome/free-solid-svg-icons";
+import Navigation from "../Navigation";
+import CartIcon from "../CartIcon";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <StyledHeader>
+      <Logo to="/" title="Go to Home" reloadDocument>
+        <FontAwesomeIcon icon={faCreditCard} size="2x" />
+        <span>SHOP</span>
+      </Logo>
+      <Navigation />
+      <MenuContainer>
+        <CartIcon />
+        <MenuButton title="Open Navigation">
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </MenuButton>
+      </MenuContainer>
+    </StyledHeader>
+  );
 };
 
 export default Header;
