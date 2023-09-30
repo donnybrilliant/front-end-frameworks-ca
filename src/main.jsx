c3134709baea080237d65abb9a5b40db0644f982
donnybrilliant <daniel.vier@gmail.com>
2023-09-30 15:38:47 +0200

    Adds GlobalStyle and Theme to main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Theme, GlobalStyle } from "./components/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Theme>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
   </React.StrictMode>
 );
