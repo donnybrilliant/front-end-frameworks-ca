ef7ea75b3887e54d0e05c22e206536a6dc9741c3
donnybrilliant <daniel.vier@gmail.com>
2023-09-30 15:08:38 +0200

    Updates Product Page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,19 @@
+import { useParams } from "react-router-dom";
+import useApi from "../../hooks/useApi";
+import SingleProduct from "../../components/SingleProduct";
+
 const Product = () => {
-  return <div>Product</div>;
+  let { id } = useParams();
+  const { data, isLoading, isError } = useApi(
+    `https://api.noroff.dev/api/v1/online-shop/${id}`
+  );
+  return (
+    <main>
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>Error loading data</div>}
+      {data && <SingleProduct product={data} />}
+    </main>
+  );
 };
 
 export default Product;
