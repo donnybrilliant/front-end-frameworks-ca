2723201ce82c291143ae3bfb681d2c4a9e9dc410
donnybrilliant <daniel.vier@gmail.com>
2023-09-30 17:23:29 +0200

    Adds counter to CartIcon

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -2,50 +2,45 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const StyledHeader = styled.header`
-    background-color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  background-color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    &  a:hover {
-            transform: scale(1.2);
-        
-    }
-    
+  & a:hover {
+    transform: scale(1.2);
+  }
 `;
 
 export const Logo = styled(Link)`
-    text-decoration: none;
-    padding: 1rem;
-    display: flex;
-    align-items: center;
-    & > i {
-        margin-right: 0.5rem;
-
-
-    }
-    & > span {
-        font-family: 'IBM Plex Mono', Arial, Helvetica, sans-serif;
-   
+  text-decoration: none;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  & > svg {
+    margin-right: 0.5rem;
+  }
+  & > span {
+    font-family: "IBM Plex Mono", Arial, Helvetica, sans-serif;
 
     @media (max-width: 270px) {
-    display: none;
-    }}
+      display: none;
+    }
+  }
 `;
 
 export const MenuContainer = styled.div`
-    display: flex;
+  display: flex;
 `;
 
 export const MenuButton = styled.button`
-    padding: 1rem;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    width: 4rem; 
-  
+  padding: 1rem;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  width: 4rem;
 
-    @media (min-width: 600px) {
-        display: none;
-    }
+  @media (min-width: 600px) {
+    display: none;
+  }
 `;
