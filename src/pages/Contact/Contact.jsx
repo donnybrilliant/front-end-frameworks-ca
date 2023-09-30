80945eabe4025c55091bce03a59747374152f477
donnybrilliant <daniel.vier@gmail.com>
2023-09-30 16:26:46 +0200

    Updates Contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import Button from "../../components/Button";
+import { StyledForm } from "./Contact.styled";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    fullName: "",
+    subject: "",
+    email: "",
+    body: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form validation here
+    // Log the form data once validation requirements are met
+    console.log(formData);
+  };
+
+  return (
+    <main>
+      <h1>Contact Us</h1>
+      <StyledForm onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            name="fullName"
+            value={formData.fullName}
+            onChange={handleChange}
+            required
+            minLength="3"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="subject">Subject</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            required
+            minLength="3"
+          />
+        </div>
+        <div>
+          <label htmlFor="body">Message</label>
+          <textarea
+            id="body"
+            name="body"
+            value={formData.body}
+            onChange={handleChange}
+            required
+            minLength="3"
+            rows="10"
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </StyledForm>
+    </main>
+  );
 };
 
 export default Contact;
