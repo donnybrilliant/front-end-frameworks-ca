92189df8cafa3ead9348397fa189ecd1a13d4272
donnybrilliant <daniel.vier@gmail.com>
2023-10-06 17:45:27 +0200

    Cleans up ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,29 +1,31 @@
 import { Link } from "react-router-dom";
 import Button from "../Button";
-import { ProductContainer, StyledDiv } from "./ProductCard.styled";
+import {
+  ProductContainer,
+  PriceAndButton,
+  Heading,
+} from "./ProductCard.styled";
 import Rating from "../Rating";
 import { isOnSale } from "../../utils";
 
 const ProductCard = ({ product }) => {
   return (
     <ProductContainer key={product.id}>
-      <div>
+      <Heading>
         <h2>
           <Link to={`/product/${product.id}`}>{product.title}</Link>
         </h2>
         {isOnSale(product) && <span>ON SALE</span>}
-      </div>
-
+      </Heading>
       <img src={product.imageUrl} alt={product.title} />
       <Rating rating={product.rating} />
       {isOnSale(product) && <s>${product.price}</s>}
-      <StyledDiv>
+      <PriceAndButton>
         <p>${product.discountedPrice}</p>
-
         <Link to={`/product/${product.id}`}>
           <Button>View Product</Button>
         </Link>
-      </StyledDiv>
+      </PriceAndButton>
     </ProductContainer>
   );
 };
--- src/components/ProductCard/ProductCard.styled.jsx
+++ src/components/ProductCard/ProductCard.styled.jsx
@@ -12,6 +12,29 @@ export const ProductContainer = styled.li`
   box-shadow: ${({ theme }) =>
     `${theme.shadows.regular} ${theme.colors.black}`};
 
+  &:hover {
+    box-shadow: ${({ theme }) =>
+      `${theme.shadows.large} ${theme.colors.black}`};
+  }
+
+  img {
+    border-radius: ${({ theme }) =>
+      `${theme.borderRadius.regular} 0 0 ${theme.borderRadius.regular}`};
+    height: 200px;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  s {
+    color: ${({ theme }) => theme.colors.warning};
+    margin-bottom: -1rem;
+  }
+`;
+export const Heading = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   h2 {
     margin-right: 1rem;
     font-size: 1.2rem;
@@ -20,37 +43,16 @@ export const ProductContainer = styled.li`
     }
   }
 
-  &:hover {
-    box-shadow: ${({ theme }) =>
-      `${theme.shadows.large} ${theme.colors.black}`};
-  }
-
-  img {
-    border-radius: 15px 0 0 15px;
-    height: 200px;
-    width: 100%;
-    object-fit: cover;
-  }
-
-  div:first-of-type {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-    span {
-      background-color: ${({ theme }) => theme.colors.attention};
-      padding: 0.5rem;
-      border-radius: 15px 0 0 15px;
-      font-size: 0.75rem;
-    }
-  }
-  s {
-    color: ${({ theme }) => theme.colors.warning};
-    margin-bottom: -1rem;
+  span {
+    background-color: ${({ theme }) => theme.colors.attention};
+    padding: 0.5rem;
+    border-radius: ${({ theme }) =>
+      `${theme.borderRadius.regular} 0 0 ${theme.borderRadius.regular}`};
+    font-size: 0.75rem;
   }
 `;
 
-export const StyledDiv = styled.div`
+export const PriceAndButton = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
@@ -67,8 +69,8 @@ export const StyledDiv = styled.div`
     display: flex;
   }
 
-  & button,
-  a {
+  a,
+  button {
     flex-grow: 1;
   }
 `;
