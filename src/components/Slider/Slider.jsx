b2fe19e0bddd0bdaa6d8c1bc1d0aaf067a60cbd0
donnybrilliant <daniel.vier@gmail.com>
2023-10-07 21:28:39 +0200

    Updates Slider to have its own state

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,36 +1,44 @@
-import { useContext, useCallback } from "react";
+import { useContext, useState, useEffect, useCallback } from "react";
 import { ThemeContext } from "../Theme";
 import { SliderInput } from "./Slider.styled";
 
 function debounce(func, delay) {
   let timer;
-  return function () {
-    const context = this;
-    const args = arguments;
+  return function (...args) {
     clearTimeout(timer);
     timer = setTimeout(() => {
-      func.apply(context, args);
+      func(...args);
     }, delay);
   };
 }
 
 const Slider = () => {
-  const { handleSliderChange } = useContext(ThemeContext);
+  const { handleSliderChange, hueShift } = useContext(ThemeContext);
+  const [sliderValue, setSliderValue] = useState(hueShift); // Local state for slider
 
-  const handleChange = (e) => {
-    const sliderValue = parseInt(e.target.value);
-    handleSliderChange(sliderValue);
+  const handleImmediateChange = (e) => {
+    const newSliderValue = parseInt(e.target.value);
+    setSliderValue(newSliderValue); // Instant update for the slider's movement
   };
 
-  const debouncedSliderChange = useCallback(debounce(handleChange, 150), []);
+  const handleDebouncedChange = useCallback(
+    debounce((value) => {
+      handleSliderChange(value);
+    }, 150),
+    [handleSliderChange]
+  );
+
+  useEffect(() => {
+    handleDebouncedChange(sliderValue); // Update theme based on debounced value
+  }, [sliderValue, handleDebouncedChange]);
 
   return (
     <SliderInput
       type="range"
       min="0"
       max="360"
-      defaultValue="0"
-      onChange={debouncedSliderChange}
+      value={sliderValue}
+      onChange={(e) => handleImmediateChange(e)}
     />
   );
 };
