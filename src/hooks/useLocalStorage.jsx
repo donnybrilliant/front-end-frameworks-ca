713f33c95d72cd73c258592142e8861d7f43a479
donnybrilliant <daniel.vier@gmail.com>
2023-10-07 21:26:48 +0200

    Adds comments to useLocalStorage

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 const useLocalStorage = (key, defaultValue) => {
+  // Retrieve the value from local storage or use the default value
   const [value, setValue] = useState(() => {
     let currentValue;
     try {
@@ -10,24 +11,30 @@ const useLocalStorage = (key, defaultValue) => {
       } else if (Array.isArray(defaultValue) && defaultValue.length === 0) {
         currentValue = defaultValue;
       } else {
+        // If no value is found in local storage, set it to the default value
         localStorage.setItem(key, JSON.stringify(defaultValue));
         currentValue = defaultValue;
       }
     } catch (error) {
+      // Handle any errors (e.g., parsing JSON)
       currentValue = defaultValue;
     }
 
     return currentValue;
   });
 
+  // Update local storage whenever the value changes
   useEffect(() => {
     if (value === undefined || (Array.isArray(value) && value.length === 0)) {
+      // Remove the key if the value is undefined or an empty array
       localStorage.removeItem(key);
     } else {
+      // Store the value in local storage
       localStorage.setItem(key, JSON.stringify(value));
     }
   }, [value, key]);
 
+  // Function to remove the value from local storage
   const removeValue = () => setValue(undefined);
 
   return [value, setValue, removeValue];
