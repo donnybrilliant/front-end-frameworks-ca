937171417edd337667fc45556d9865e41f5c5799
donnybrilliant <daniel.vier@gmail.com>
2023-10-08 21:22:34 +0200

    Adds Search to Home and adds container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,31 @@
 import useApi from "../../hooks/useApi";
+import { useState } from "react";
 import ProductList from "../../components/ProductList";
 import Loader from "../../components/Loader";
 import Error from "../../components/Error";
+import Search from "../../components/Search/Search";
+import { HomeContainer } from "./Home.styled";
 
 const Home = () => {
   const { data, isLoading, isError } = useApi(
     "https://api.noroff.dev/api/v1/online-shop"
   );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
 
   if (isLoading) return <Loader />;
 
   if (isError) return <Error>Error Loading Products</Error>;
 
-  return <>{data && <ProductList products={data} />}</>;
+  return (
+    <HomeContainer>
+      <Search onSearch={handleSearch} />
+      {data && <ProductList products={data} searchQuery={searchQuery} />}
+    </HomeContainer>
+  );
 };
 
 export default Home;
