52d2ad6b733b10f66d36a0fe566dd882b956281b
donnybrilliant <daniel.vier@gmail.com>
2023-10-08 21:22:49 +0200

    Adds filtering based on search in ProductList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,23 @@
 import ProductCard from "../ProductCard";
 import { ProductGrid } from "./ProductList.styled";
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, searchQuery }) => {
+  const filteredProducts = products.filter(
+    (product) =>
+      product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      product.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      product.tags.some((tag) => tag.includes(searchQuery.toLowerCase()))
+  );
+
   return (
     <ProductGrid>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      {filteredProducts.length === 0 ? (
+        <h2>No products found</h2>
+      ) : (
+        filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      )}
     </ProductGrid>
   );
 };
