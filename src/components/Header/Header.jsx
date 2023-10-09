559f8ccb3be9af724dc2c5c25989611764725b05
donnybrilliant <daniel.vier@gmail.com>
2023-10-09 14:54:57 +0200

    Closes mobile menu on click outside Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import Navigation from "../Navigation";
 import CartIcon from "../CartIcon";
@@ -13,13 +13,35 @@ import { StyledHeader, Logo, MenuContainer, MenuButton } from "./Header.styled";
 const Header = () => {
   const [extendedNav, setExtendedNav] = useState(false);
   const location = useLocation();
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    // Add a click event listener to the document
+    const closeMenuOnOutsideClick = (event) => {
+      // Check if the click target is within the Header component
+      if (headerRef.current && headerRef.current.contains(event.target)) {
+        return;
+      }
+
+      // If not, close the menu
+      setExtendedNav(false);
+    };
+
+    // Attach the event listener
+    document.addEventListener("click", closeMenuOnOutsideClick);
+
+    // Cleanup: Remove the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("click", closeMenuOnOutsideClick);
+    };
+  }, []);
 
   useEffect(() => {
     setExtendedNav(false);
   }, [location]);
 
   return (
-    <StyledHeader>
+    <StyledHeader ref={headerRef}>
       <Logo to="/" title="Go to Home" reloadDocument>
         <FontAwesomeIcon icon={faCreditCard} size="2x" />
         <span>SHOP</span>
